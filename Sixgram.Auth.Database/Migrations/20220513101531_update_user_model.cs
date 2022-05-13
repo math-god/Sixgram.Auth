57d21c22daff5cdb9bf4280b2ac8183c35ea6873
kraken-god <danya.dyakonov@mail.ru>
2022-05-13 15:57:44 +0300

    Added new migration

---
+++ Sixgram.Auth.Database/Migrations/20220513101531_update_user_model.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Sixgram.Auth.Database.Migrations
+{
+    public partial class update_user_model : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Firstname",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Lastname",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Firstname",
+                table: "Users",
+                type: "character varying(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Lastname",
+                table: "Users",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "character varying(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
